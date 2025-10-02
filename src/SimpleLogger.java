d460999be02d37b196c6f184534afaac7484dfb7
OlivierQc <olivier.bachand@gmail.com>
2025-10-02 14:27:01 -0400

    implemented singleton

--- src/SimpleLogger.java
+++ src/SimpleLogger.java
@@ -3,6 +3,14 @@ import java.time.format.DateTimeFormatter;
 
 public class SimpleLogger {
     private static final String DateFormat = "dd-MM-yyyy HH:mm:ss";
+    private static SimpleLogger aLogger;
+    private SimpleLogger() {}
+    public static SimpleLogger getInstance() {
+        if (SimpleLogger.aLogger == null) {
+            SimpleLogger.aLogger = new SimpleLogger();
+        }
+        return SimpleLogger.aLogger;
+    }
     public void logThis(String pLogMessage, String pLogLevel) {
         String logEntry = String.format("[%s] [%s] %s", pLogLevel, LocalDateTime.now().format(DateTimeFormatter.ofPattern(DateFormat)), pLogMessage);
         System.out.println(logEntry);
